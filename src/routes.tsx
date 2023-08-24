c6675330274c697d15a8d7bec94af19fc9fc0af1
Matheus Ferretti <matheus10.ferretti@gmail.com>
2023-08-24 16:28:56 -0300

    Fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./containers/Home";
-import Inverno from "./containers/Inverno";
 import Verao from "./containers/catalogo";
 
 const Rotas = () => {
@@ -9,7 +8,6 @@ const Rotas = () => {
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalogo" element={<Verao/>} />
-        <Route path="/inverno" element={<Inverno/>} />
     </Routes>
     )
     
